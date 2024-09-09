846ad1504fed7947bbd7556e53fb9b3d19b14461
NeverStepD7 <NeverStepD7@gmail.com>
2024-09-09 11:21:14 -0700

    weapons

--- Assets/Gricel/Finished/Eneny states scripts/EnemyGun.cs
+++ Assets/Gricel/Finished/Eneny states scripts/EnemyGun.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class EnemyGun : MonoBehaviour
 {
-	[SerializeField] Countdown shoot_firerate = new(0.2f);
+	[SerializeField] ShootBehavior[] weapons;
+	Countdown shooting = new(0.2f);
+
+	private void Start()
+	{
+		shooting.Countdown_ForceSeconds(-1);
+	}
 	public void Shoot()
 	{
-		if (shoot_firerate.CountdownReturn())
-			Debug.Log("Pew");
-
+		shooting.Countdown_Restart();
+	}
+	private void Update()
+	{
+		foreach (var weapon in weapons)
+		{
+			if (shooting.CountdownReturn())
+				weapon.StartShooting();
+			else weapon.StopShooting();
+		}
 	}
 }
